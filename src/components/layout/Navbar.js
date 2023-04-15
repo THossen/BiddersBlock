e1631d0742ed0b7e0bf6a03e8460422e64ae4b83
THossen <tan.hossen.1@gmail.com>
2023-04-15 18:08:30 -0400

    removed Landing page from nav component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <header className="bg-blue-300 flex justify-between items-center px-4 py-4">
-            <nav className="space-x-4 md.block">
-                <Link to="/"> Welcome </Link>
+            <Link to="/"> Welcome </Link>
+            <nav className="space-x-4 md.block font-bold">
                 <Link to="/Home" className="ml-auto"> Home </Link>  
             </nav>
         </header>
