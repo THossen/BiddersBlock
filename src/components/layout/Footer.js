df6f24f0372782c33f4427a1ab4c74355ded7843
THossen <tan.hossen.1@gmail.com>
2023-04-16 04:21:25 -0400

    Added footer componenet

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,13 @@
 const Footer = () => {
     return (
-        <div>
-            Footer section
-        </div>
+        <footer className="bg-violet-950 text-white text-xl font-bold justify-between items-center px-4 py-6 space-y-2">
+            <div>
+                About us
+            </div>
+            <div>
+                Contact us
+            </div>
+        </footer>
     )
 }
 
