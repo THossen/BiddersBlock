e0c3695549e3fdacdee14da12c7b25278853f0dc
THossen <tan.hossen.1@gmail.com>
2023-04-23 19:49:52 -0400

    Added api call to Login component

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -2,19 +2,32 @@ import React, { useState, useContext } from "react";
 import AuthContext from "../../auth/AuthContext";
 import LoginForm from "./LoginForm";
 
-const Login = ({ username, password }) => {
+const Login = () => {
   const [usernameInput, setUsernameInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const { login } = useContext(AuthContext);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    if (usernameInput === username && passwordInput === password) {
+    const response = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userName: usernameInput,
+        userPassword: passwordInput
+      })
+    });
+
+    if (response.ok) {
       login();
       setUsernameInput("");
       setPasswordInput("");
-    } else {
+    } else if (response.status === 401) {
       alert("Invalid username or password.");
+    } else {
+      alert("Failed to login.");
     }
   };
 
@@ -32,3 +45,4 @@ const Login = ({ username, password }) => {
 };
 
 export default Login;
+
