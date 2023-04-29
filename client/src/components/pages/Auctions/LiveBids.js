7b59b3f51ea0af8eed19d72db52698256b1c8b25
Robert Chu <robertc2468@gmail.com>
2023-04-29 17:27:15 -0400

    Fixed LiveBids to display the live bids

--- client/src/components/pages/Auctions/LiveBids.js
+++ client/src/components/pages/Auctions/LiveBids.js
@@ -1,32 +1,31 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const LiveBids = ({ itemID }) => {
   const [bids, setBids] = useState([]);
 
   useEffect(() => {
-    const fetchBids = async () => {
-      try {
-        const res = await axios.get(`/latest-bids`);
-        setBids(res.data.bids);
-      } catch (err) {
-        console.error(err);
-      }
+    const fetchBids = () => {
+      axios.get(`http://localhost:3001/latest-bids/${itemID}`)
+        .then(res => {
+          setBids(res.data.bids);
+        })
+        .catch(err => {
+          console.error(err);
+        });
     };
 
     fetchBids();
-    const interval = setInterval(() => {
-      fetchBids();
-    }, 50000); 
+    const interval = setInterval(fetchBids, 5000);
 
     return () => clearInterval(interval);
   }, [itemID]);
 
   return (
     <div>
-      <h3 className='font-bold text-3xl'>Latest Bids:</h3>
-      {bids.map((bid) => (
-        <p key={bid.bidID}>
+      <h3>Latest Bids:</h3>
+      {bids.map(bid => (
+        <p key={bid.itemID}>
           User {bid.bidderID} bid ${bid.bidAmount} at {bid.bid_time}
         </p>
       ))}
