0d910d2c6d97c7ab5f3237783b0a8187c249c04a
Robert Chu <robertc2468@gmail.com>
2023-04-29 22:48:02 -0400

    Clean up UserInfo and apply CSS to AboutUs

--- client/src/components/pages/Profile/UserInfo.js
+++ client/src/components/pages/Profile/UserInfo.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import useAuth from "../../Providers/useAuth";
-import axios from 'axios';
-
 
 const UserInfo = () => {
   const { user } = useAuth();
   console.log(user);
 
   return (
-    <div className="rounded-lg shadow-lg w-full h-full">
-      <h2 className="text-4xl font-bold text-gray-800 md:text-5xl">
+    <div className="rounded-lg shadow-lg w-full h-full p-4">
+      <h2 className="text-4xl font-bold text-gray-800 md:text-5xl mb-4">
         User Info
       </h2>
       <div className="text-1x1 text-gray-800 text-center py-1">
-        <table>
+        <table className="w-full border-collapse">
           <tr>
-            <td text-align='center'>{user ? user.userFirstName : "First Name"} {user ? user.userLastName : "Last Name"}</td>
+            <td className="border px-4 py-2 text-left font-medium">Name:</td>
+            <td className="border px-4 py-2 text-right">{user ? user.userFirstName : "First Name"} {user ? user.userLastName : "Last Name"}</td>
           </tr>
           <tr>
-            <td align='left'>Email: </td>
-            <td align='right'>{user ? user.userEmail : "Email"}</td>
+            <td className="border px-4 py-2 text-left">Email: </td>
+            <td className="border px-4 py-2 text-right">{user ? user.userEmail : "Email"}</td>
           </tr>
           <tr>
-            <td>Address: </td>
-            <td align='right'>{user ? user.userAddress : "Address"}</td>
+            <td className="border px-4 py-2 text-left">Address: </td>
+            <td className="border px-4 py-2 text-right">{user ? user.userAddress : "Address"}</td>
           </tr>
         </table>
-        </div>
+      </div>
     </div>
   )
 }
