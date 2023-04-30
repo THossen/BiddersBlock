eebcba30318ec104a9257fe0e897d5a0c52354cc
THossen <tan.hossen.1@gmail.com>
2023-04-29 21:03:09 -0400

    formatted date and time for live bids display

--- client/src/components/pages/Auctions/LiveBids.js
+++ client/src/components/pages/Auctions/LiveBids.js
@@ -21,12 +21,25 @@ const LiveBids = ({ itemID }) => {
     return () => clearInterval(interval);
   }, [itemID]);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { 
+      month: 'short', 
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    };
+    return date.toLocaleString('en-US', options);
+  };
+
   return (
     <div>
       <h3>Latest Bids:</h3>
       {bids.map(bid => (
         <p key={bid.itemID}>
-          User {bid.bidderID} bid ${bid.bidAmount} at {bid.bid_time}
+          User {bid.bidderID} bid ${bid.bidAmount} at {formatDate(bid.bid_time)}
         </p>
       ))}
     </div>
