b442dffb750b60ff2e05b6e09d88a3a15c29bf2a
OrtegaIsaac <iortega@nyit.edu>
2023-04-29 22:13:26 -0400

    Diplays user info

--- client/src/components/pages/Profile/UserInfo.js
+++ client/src/components/pages/Profile/UserInfo.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import useAuth from "../../Providers/useAuth";
+import axios from 'axios';
+
 
 const UserInfo = () => {
+  const { user } = useAuth();
+  console.log(user);
+
   return (
-    <div className="font-bold text-5xl text-center py-8">User Info</div>
+    <div className="rounded-lg shadow-lg w-full h-full">
+      <h2 className="text-4xl font-bold text-gray-800 md:text-5xl">
+        User Info
+      </h2>
+      <div className="text-1x1 text-gray-800 text-center py-1">
+        <table>
+          <tr>
+            <td text-align='center'>{user ? user.userFirstName : "First Name"} {user ? user.userLastName : "Last Name"}</td>
+          </tr>
+          <tr>
+            <td align='left'>Email: </td>
+            <td align='right'>{user ? user.userEmail : "Email"}</td>
+          </tr>
+          <tr>
+            <td>Address: </td>
+            <td align='right'>{user ? user.userAddress : "Address"}</td>
+          </tr>
+        </table>
+        </div>
+    </div>
   )
 }
 
