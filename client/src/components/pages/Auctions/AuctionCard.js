80cf404fd9eba5ac5fa5de9f86a6adaae48e1454
THossen <tan.hossen.1@gmail.com>
2023-05-01 17:17:07 -0400

    prevent user from bidding and accessing ended auctions

--- client/src/components/pages/Auctions/AuctionCard.js
+++ client/src/components/pages/Auctions/AuctionCard.js
@@ -6,17 +6,27 @@ function AuctionCard(props) {
     highestPrice,
     auctionStartTime,
     auctionEndTime,
+    isAuctionEnded,
   } = props;
   const startTime = new Date(auctionStartTime);
   const endTime = new Date(auctionEndTime);
 
+  const overlayClass = isAuctionEnded
+    ? "absolute inset-0 bg-gray-900 opacity-50"
+    : "";
+  const soldClass = isAuctionEnded
+    ? "absolute inset-0 flex items-center justify-center text-4xl font-bold text-white"
+    : "hidden";
+
   return (
-    <div className="rounded overflow-hidden shadow-lg h-full">
+    <div className="rounded overflow-hidden shadow-lg h-full relative">
       <img
         className="w-full h-96 object-cover"
         src={itemPicture}
         alt={itemName}
       />
+      <div className={overlayClass}></div>
+      <div className={soldClass}>SOLD</div>
       <div className="px-6 py-6">
         <div className="font-bold text-xl mb-2">{itemName}</div>
         <p className="text-gray-700 text-base">{itemDescription}</p>
--- client/src/components/pages/Auctions/Auctions.js
+++ client/src/components/pages/Auctions/Auctions.js
@@ -10,22 +10,36 @@ const Auctions = () => {
     fetchData();
   }, [fetchData]);
 
+  const renderAuctionCard = (auction) => {
+    const currentTime = new Date();
+    const endTime = new Date(auction.auctionEndTime);
+    const isAuctionEnded = endTime <= currentTime;
+
+    const auctionCard = (
+      <AuctionCard
+        key={auction.itemID}
+        itemName={auction.itemName}
+        itemDescription={auction.itemDescription}
+        itemPicture={auction.itemPicture}
+        highestPrice={auction.highestPrice}
+        auctionStartTime={auction.auctionStartTime}
+        auctionEndTime={auction.auctionEndTime}
+        isAuctionEnded={isAuctionEnded}
+      />
+    );
+
+    return isAuctionEnded ? (
+      auctionCard
+    ) : (
+      <Link key={auction.itemID} to={`/auctions/${auction.itemID}`}>
+        {auctionCard}
+      </Link>
+    );
+  };
+
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
-      {auctionData.map((auction) => (
-        <Link key={auction.itemID} to={`/auctions/${auction.itemID}`}>
-          <div>
-            <AuctionCard
-              itemName={auction.itemName}
-              itemDescription={auction.itemDescription}              
-              itemPicture={auction.itemPicture}
-              highestPrice={auction.highestPrice}
-              auctionStartTime={auction.auctionStartTime}
-              auctionEndTime={auction.auctionEndTime}
-            />
-          </div>
-        </Link>
-      ))}
+      {auctionData.map((auction) => renderAuctionCard(auction))}
     </div>
   );
 };
