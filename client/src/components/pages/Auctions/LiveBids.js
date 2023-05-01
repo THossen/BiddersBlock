71e44256fd2e19ea12b44595e3c4116e503dd942
THossen <tan.hossen.1@gmail.com>
2023-05-01 18:07:07 -0400

    styled livebids and tested with two user instances

--- client/src/components/pages/Auctions/LiveBids.js
+++ client/src/components/pages/Auctions/LiveBids.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const LiveBids = ({ itemID }) => {
   const [bids, setBids] = useState([]);
 
   useEffect(() => {
     const fetchBids = () => {
-      axios.get(`http://localhost:3001/latest-bids/${itemID}`)
-        .then(res => {
+      axios
+        .get(`http://localhost:3001/latest-bids/${itemID}`)
+        .then((res) => {
           setBids(res.data.bids);
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(err);
         });
     };
@@ -23,25 +24,32 @@ const LiveBids = ({ itemID }) => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { 
-      month: 'short', 
-      day: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true
+    const options = {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
     };
-    return date.toLocaleString('en-US', options);
+    return date.toLocaleString("en-US", options);
   };
 
   return (
-    <div>
-      <h3>Latest Bids:</h3>
-      {bids.map(bid => (
-        <p key={bid.itemID}>
-          User {bid.bidderID} bid ${bid.bidAmount} at {formatDate(bid.bid_time)}
-        </p>
-      ))}
+    <div className="w-full max-w-md mx-auto">
+      <h3 className="text-2xl font-semibold text-center mb-5">Latest Bids:</h3>
+      <div className="space-y-3">
+        {bids.map((bid) => (
+          <div
+            key={bid.itemID}
+            className="flex justify-between items-center p-3 border border-gray-300 rounded-md"
+          >
+            <span className="font-medium">User {bid.bidderID}</span> bid{" "}
+            <span className="font-medium text-cyan-500">${bid.bidAmount}</span>{" "}
+            at <span className="text-gray-500">{formatDate(bid.bid_time)}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
